ee8307f3b1a5a44e56d3066ccb05744a0b2aa37c
Alfonsoq6 <64447470+Alfonsoq6@users.noreply.github.com>
2020-10-26 14:04:20 -0700

    Add files via upload

---
+++ routes/apiRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const storeNotes = require("../db/storeNotes.js");
+
+// API get request for retrieving existing notes
+router.get("/notes", function (req, res) {
+  storeNotes
+    .retrieveNotes()
+    .then(notes => res.json(notes))
+
+    .catch(err => res.status(500).json(err));
+});
+
+// API post request for posting a new note
+router.post("/notes", (req, res) => {
+  storeNotes
+    .addNote(req.body)
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
+
+// API delete request for deleting a note
+router.delete("/notes/:id", function (req, res) {
+  storeNotes
+    .removeNote(req.params.id)
+    .then(() => res.json({ success: true }))
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const router = require("express").Router();
+
+// HTML GET Requests
+
+router.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// If no matching route is found default to index
+router.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
